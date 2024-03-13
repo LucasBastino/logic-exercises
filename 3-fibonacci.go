befecaa52c20f1df769e81e4ee8dea8ce4fa371a
Lucas Bastino <lucasbastino.dev@gmail.com>
2024-03-12 21:56:53 -0300

    3 y 4 agregados

--- 3-fibonacci.go
+++ 3-fibonacci.go
@@ -1,7 +1,22 @@
 package main
 
-func main() {
-	for i:=0; i<50; i++{
-		sumaTotal += 
+import "fmt"
+
+func ejercicio3() {
+	// secuencia := make([]int, 50)
+	secuencia := [50]int{0, 1}
+	for i := 2; i < 50; i++ {
+		secuencia[i] = secuencia[i-1] + secuencia[i-2]
 	}
+	fmt.Println(secuencia)
+
+	// numero0 := 0
+	// numero1 := 1
+
+	// for i := 0; i < 50; i++ {
+	// 	fmt.Println(numero0)
+	// 	sumaTotal := numero0 + numero1
+	// 	numero0 = numero1
+	// 	numero1 = sumaTotal
+	// }
 }
---
+++ 4-numerosPrimos.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	//		for n := 1; n < 200; n++ {
+	//			if esPrimo(n) {
+	//				fmt.Println(n)
+	//			}
+	//		}
+	//	}
+	fmt.Println(1%0 == 0)
+}
+
+// func esPrimo(n int) bool {
+// 	for i:=0; i<n; i++{
+// 		if n%i == 0{
+
+// 		}
+// 	}
+// }
